6f8eb61633aea6a7ea168c05cf9f1306ad8181b1
Sant <sganis@gmail.com>
2016-05-13 19:06:51 +0300

    Added v0.3 with continuous array to test performance

--- terapressure-0.3.c
+++ terapressure-0.3.c
@@ -1,5 +1,5 @@
 /*********************************************************
-* Terapressure 0.2
+* Terapressure 0.3
 * Exercise MPI course on 05/20/2016
 * Author: Santiago
 * Date: That weekend.
@@ -26,21 +26,29 @@
 * Time elapsed: 0.01 seconds.
 **********************************************************/
 
+#define VERSION "v0.3"
+
 #include <stdio.h>
 #include <stdlib.h> // malloc, exit
-#include "mpi.h"
+#include <mpi.h>
 
 int main(int argc, char *argv[])
 {
-  long N=8, M=8;        // number of cells NxM
-  int n=2,  m=2;        // number of blocks nxm
-  int tpi=3, tpj=3;     // test pressure coordinates
-  int tai=3, taj=3;     // test average coordinates
+  int N=20, M=30;      // number of cells NxM
+  int n=2,  m=3;        // number of blocks nxm
+  int tpi=16, tpj=18;   // test pressure coordinates
+  int tai=7, taj=9;     // test average coordinates
+  //int N=8, M=8;      // number of cells NxM
+  //int n=2,  m=2;        // number of blocks nxm
+  //int tpi=3, tpj=3;   // test pressure coordinates
+  //int tai=3, taj=3;     // test average coordinates
+  
   int i, j, I, J;       // local and global i,j
   int myi, myj;         // my i,j in neighbor map
   int by, bx;           // block size in y and x direction
   int numprocs, myid;   // number of processors and my rank id
-  double **P, **A;      // 2D array of pressures and averages
+  double *P, *A;        // 2D array of pressures and averages
+                        // (1D continues array for performance)
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
@@ -72,15 +80,19 @@ int main(int argc, char *argv[])
 
   by = N/n;
   bx = M/m;
+  int width = bx + 2;
+  int height = by + 2;
 
   // start message
   if (myid==0) {
-    printf("Terapressure v0.2\n");
+    printf("Terapressure %s\n", VERSION);
     printf("=================\n");
     printf("Number of cells: %lu (%lu x %lu)\n", N*M, N, M);
     printf("Number of blocks: %d (%d x %d)\n", n*m, n, m);
     printf("Number of processors %d\n", numprocs);
     printf("Block size: (%d x %d)\n", by, bx);
+    printf("Block size extended (height,width): (%d x %d)\n", height, width);
+    printf("Aprox. memory needed per processor: %.2f GB\n", height*width*2*sizeof(double*)/1024.0/1024.0/1024.0);
   }
 
   // validate parameters
@@ -102,20 +114,22 @@ int main(int argc, char *argv[])
   double t = MPI_Wtime();
 
   // memory allocation
-  P = malloc(sizeof(double*) * (by + 2));
-  for (i=0; i < by+2; i++)
-    P[i] = malloc(sizeof(double) * (bx + 2));
-
-  A = malloc(sizeof(double*) * (by + 2));
-  for (i=0; i < by+2; i++)
-    A[i] = malloc(sizeof(double) * (bx + 2));
-
+  P = malloc(sizeof(double*) * width * height);
+  A = malloc(sizeof(double*) * width * height);
   int B[n][m];
+  // P = malloc(sizeof(double*) * (by + 2) * (bx + 2));
+  // for (i=0; i < by+2; i++)
+  //   P[i] = malloc(sizeof(double) * (bx + 2));
+  // A = malloc(sizeof(double*) * (by + 2));
+  // for (i=0; i < by+2; i++)
+  //   A[i] = malloc(sizeof(double) * (bx + 2));
 
   // initialize
-  for (i=0; i < by+2; i++)
-    for (j=0; j < bx+2; j++)
-      P[i][j] = 0;
+  for (i=0; i < width * height; i++) {
+    P[i] = 0;
+    A[i] = 0;
+  }
+
 
   // domain decomposition
   int rank = 0;
@@ -137,27 +151,29 @@ int main(int argc, char *argv[])
     for (j=1; j < bx+1; j++) {
       J = myj * bx + j - 1;
       if (I==0 || I==N-1 || J==0 || J==M-1)
-        P[i][j] = 0;
+        P[i*width+j] = 0;
       else
-        P[i][j] = (double)(I+J) * (double)(I*J);
+        P[i * width + j] = (double)(I+J) * (double)(I*J);
       if (I == tpi && J == tpj)
-        pressure = P[i][j];
+        pressure = P[i * width + j];
     }
   }
-
-
-  // if (myid==0) {
-  //   for (i=0; i < by+2; i++) {
-  //     for (j=0; j < bx+2; j++)
-  //       printf("%5.0f",P[i][j]);
-  //     printf("\n");
-  //   }
-  //   printf("\n");
-  // }
+  // print pressures
+  /*
+  if (myid==0) {
+    printf("%d: Pressures:\n", myid);
+    for (i=0; i < width * height; i++) {
+        printf("%5.0f", P[i]);
+        if ((i+1) % width == 0)
+          printf("\n");
+    }
+    printf("\n");
+  }
+  */
 
   // average pressure
-  double send_x[bx];
-  double recv_x[bx];
+  //double send_x[bx];
+  //double recv_x[bx];
   double send_y[by];
   double recv_y[by];
 
@@ -170,46 +186,60 @@ int main(int argc, char *argv[])
 
   // top row
   if (Is > 0) {
-    for (j=1; j < bx+1; j++)
-      send_x[j-1] = P[1][j];
+    //for (j=1; j < bx+1; j++)
+    //  send_x[j-1] = P[1* width + j];
     b = B[myi-1][myj];
     if ((myi+myj) % 2 == 0) {
       // printf("%2d: send %d top row\n", myid, b);
-      MPI_Send(send_x, bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD);
+      MPI_Send(&P[width+1], bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD);
       // printf("%2d: recv %d bottom row\n", myid, b);
-      MPI_Recv(recv_x, bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(&P[0 * width + 1], bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     } else {
       // printf("%2d: recv %d bottom row\n", myid, b);
-      MPI_Recv(recv_x, bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(&P[0*width+1], bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       // printf("%2d: send %d top row\n", myid, b);
-      MPI_Send(send_x, bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD);
+      MPI_Send(&P[width+1], bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD);
     }
-    for (j=1; j < bx+1; j++)
-      P[0][j] = recv_x[j-1];
+    //for (j=1; j < bx+1; j++)
+    //  P[0 * width + j] = recv_x[j-1];
   }
   // bottom row
   if (Ie < N-1) {
-    for (j=1; j < bx+1; j++)
-      send_x[j-1] = P[by][j];
+    //for (j=1; j < bx+1; j++)
+    //  send_x[j-1] = P[by * width + j];
+    //if(myid==0) {
+    //  printf("%d: bufer to send:\n", myid);
+    //  for (j=1; j < bx+1; j++)
+    //    printf("%5.0f", send_x[j-1]);
+    //  printf("\n");
+    //}
     b = B[myi+1][myj];
     if ((myi+myj) % 2 == 0) {
       // printf("%2d: send %d bottom row\n", myid, b);
-      MPI_Send(send_x, bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD);
+      MPI_Send(&P[by*width+1], bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD);
       // printf("%2d: recv %d top row\n", myid, b);
-      MPI_Recv(recv_x, bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(&P[(by+1)*width+1], bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     } else {
       // printf("%2d: recv %d top row\n", myid, b);
-      MPI_Recv(recv_x, bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(&P[(by+1)*width+1], bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       // printf("%2d: send %d bottom row\n", myid, b);
-      MPI_Send(send_x, bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD);
+      MPI_Send(&P[by*width+1], bx, MPI_DOUBLE, b, 0, MPI_COMM_WORLD);
     }
-    for (j=1; j < bx+1; j++)
-      P[by+1][j] = recv_x[j-1];
+    //for (j=1; j < bx+1; j++)
+    //  P[(by+1) * width + j] = recv_x[j-1];
+    //if(myid==0) {
+    //  printf("%d: bufer recieved:\n", myid);
+    //  for (j=1; j < bx+1; j++)
+    //    printf("%5.0f", recv_x[j-1]);
+    //  printf("\n");
+    //}
+
+
   }
   // left column
   if (Js > 0) {
     for (i=1; i < by+1; i++)
-      send_y[i-1] = P[i][1];
+      send_y[i-1] = P[i * width + 1];
     b = B[myi][myj-1];
     if ((myi+myj) % 2 == 0) {
       // printf("%2d: send %d left column\n", myid, b);
@@ -223,12 +253,12 @@ int main(int argc, char *argv[])
       MPI_Send(send_y, by, MPI_DOUBLE, b, 0, MPI_COMM_WORLD);
     }
     for (i=1; i < by+1; i++)
-      P[i][0] = recv_y[i-1];
+      P[i * width + 0] = recv_y[i-1];
   }
   // right column
   if (Je < M-1) {
     for (i=1; i < by+1; i++)
-      send_y[i-1] = P[i][bx];
+      send_y[i-1] = P[i * width + bx];
     b = B[myi][myj+1];
     if ((myi+myj) % 2 == 0) {
       // printf("%2d: send %d right column\n", myid, b);
@@ -242,18 +272,21 @@ int main(int argc, char *argv[])
       MPI_Send(send_y, by, MPI_DOUBLE, b, 0, MPI_COMM_WORLD);
     }
     for (i=1; i < by+1; i++)
-      P[i][bx+1] = recv_y[i-1];
+      P[i * width + (bx+1)] = recv_y[i-1];
   }
 
-  // if (myid==2) {
-  //   for (i=0; i < by+2; i++) {
-  //     for (j=0; j < bx+2; j++) {
-  //       printf("%5.0f",P[i][j]);
-  //     }
-  //     printf("\n");
-  //   }
-  //   printf("\n");
-  // }
+  // print extended presures
+  /*
+  if (myid==0) {
+    printf("%d: Extended pressures:\n", myid);
+      for (i=0; i < width * height; i++) {
+        printf("%5.0f", P[i]);
+        if ((i+1) % width == 0)
+          printf("\n");
+     }
+     printf("\n");
+  }
+  */
 
   double average = -1;
 
@@ -263,24 +296,76 @@ int main(int argc, char *argv[])
       J = myj * bx + j - 1;
       if ( I==0 || I==N-1 || J==0 || J==M-1 )
         continue;
-      A[i][j] = ( P[i][j] + P[i-1][j] + P[i+1][j] + P[i][j-1] + P[i][j+1] ) / 5;
+      //A[i][j] = ( P[i][j] + P[i-1][j] + P[i+1][j] + P[i][j-1] + P[i][j+1] ) / 5;
+      A[i * width + j] = ( P[i*width+j] + P[(i-1)*width+j] + P[(i+1)*width+j] + P[i*width+(j-1)] + P[i*width+(j+1)] ) / 5;
       if (I==tai && J==taj)
-        average = A[i][j];
+        average = A[i * width + j] ;
     }
   }
 
-  // cleanup memory
+  // test
+  /*
+  int ok = 1;
+  double expected_avg = 0.0;
+
   for (i=0; i < by+2; i++) {
-    free(P[i]);
-    free(A[i]);
+    I = myi * by + i - 1;
+    for (j=0; j < bx+2; j++) {
+      J = myj * bx + j - 1;
+      if ( I<=0 || I>=N-1 || J<=0 || J>=M-1 )
+        continue;
+      P[i * width + j] = (double)(I+J) * (double)(I*J);
+    }
   }
+  
+  if (myid==0) {
+    printf("%d: Extended pressures:\n", myid);
+      for (i=0; i < width * height; i++) {
+        printf("%5.0f", P[i]);
+        if ((i+1) % width == 0)
+          printf("\n");
+     }
+     printf("\n");
+  }
+  
+  for (i=1; i < by+1; i++) {
+    I = myi * by + i - 1;
+    for (j=1; j < bx+1; j++) {
+      J = myj * bx + j - 1;
+      if ( I==0 || I==N-1 || J==0 || J==M-1 )
+        continue;
+      expected_avg = A[i * width + j];
+      A[i * width + j] = ( P[i*width+j] + P[(i-1)*width+j] + P[(i+1)*width+j] + P[i*width+(j-1)] + P[i*width+(j+1)] ) / 5;
+      if (myid==0 && expected_avg != A[i * width + j]) {
+        printf("%d: Average incorrect at (%d,%d): expected: %.2f, actual: %.2f\n",
+                 myid,I,J,expected_avg,A[i * width + j]);
+        ok = 0;
+        break;
+      }
+    }
+    if (!ok)
+      break;
+  }
+  */
+
+  // print averages
+  /*
+  if (myid==0) {
+    printf("%d: Averages:\n", myid);
+    for (i=0; i < width * height; i++) {
+        printf("%8.2f", A[i]);
+        if ((i+1) % width == 0)
+          printf("\n");
+    }
+    printf("\n");
+  }
+  */
+
+  // cleanup memory
   free(P);
   free(A);
 
-
   // report result
-  //printf("Preasure at (16,18): %.2f\n", P[16][18]);
-  //printf("Avg at (7,9): %.2f\n", A[7][9]);
   if (pressure > -1)
     printf("Preasure at (%2d,%2d): %.2f computed by processor %d\n",
       tpi, tpj, pressure, myid);
